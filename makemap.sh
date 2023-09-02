340d97597a88cfac89e9ab6183ae0d20b946917c
Jared Buckner <jaredbuckner@gmail.com>
2023-09-02 09:27:14 -0400

    Update makemap for more mountain-y settings

--- makemap.sh
+++ makemap.sh
@@ -3,17 +3,17 @@
 export PYTHONPATH=src
 
 python -midrisi.cs \
-       --separate 43 \
-       --river_source_grade 5 \
+       --separate 29 \
+       --river_source_grade 7 \
        --river_min_grade 0.0 \
        --river_separation 1100 \
        --river_segment_length 180 \
-       --shore_grade 2 \
+       --shore_grade 4 \
        --flood_plain_grade 1 \
-       --flood_plain_width 100 \
-       --foothill_width 250 \
+       --flood_plain_width 50 \
+       --foothill_width 75 \
        --foothill_grade 5 \
-       --midhill_width 90 \
+       --midhill_width 95 \
        --midhill_grade 10 \
        --shoulder_width 90 \
        --shoulder_grade 35 \
